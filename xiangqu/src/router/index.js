ca968622860e95970897833bb54232ff76e189bc
myliangxia <710056191@qq.com>
2018-11-26 10:04:32 +0800

    修改详情页

--- xiangqu/src/router/index.js
+++ xiangqu/src/router/index.js
@@ -44,7 +44,7 @@ export default new Router({
       component:Hot
     },
     {
-      path:'/details/:id',
+      path:'/details',
       name:'Details',
       component:Details,
       props:true
